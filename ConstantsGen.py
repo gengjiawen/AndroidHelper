c3ca2a6b1f1b3bec469a7ed18a2b6f4cca7b03a4
gengjiawen <technicalcute@gmail.com>
2014-10-14 20:31:25 +0800

    bug fix

--- ConstantsGen.py
+++ ConstantsGen.py
@@ -1,5 +1,5 @@
 
-s = ["activity_id", "friendName", "avatar", "title", "bg", "interstedNo", "joinNo", "isInterested"]
+s = []
 result = []
 for i in s:
     result.append("private static final String {0} = \"{1}\";".format(i.upper(), i))
--- HtmlGen.py
+++ HtmlGen.py
@@ -1,7 +1,7 @@
-name = 'tip'
+name = 'dailyLogin'
 content = '<b>Hello World</b>'
 s = '<string name="{0}"><![CDATA[{1}]]></string>'.format(name, content)
-c = 'tipTextView.setText(Html.fromHtml(getString(R.string.{0})));'.format(name)
+c = '{0}TextView.setText(Html.fromHtml(getString(R.string.{0})));'.format(name)
 
 print(s)
 print(c)
