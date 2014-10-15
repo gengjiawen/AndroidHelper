76b54f84b91c57b1d7bbdf3c343386dc312789fb
gengjiawen <technicalcute@gmail.com>
2014-10-15 09:27:02 +0800

    code enhancement

--- HtmlGen.py
+++ HtmlGen.py
@@ -1,6 +1,6 @@
 name = 'dailyLogin'
 content = '<b>Hello World</b>'
-s = '<string name="{0}"><![CDATA[{1}]]></string>'.format(name, content)
+s = '<string name="{0}" formatted="false"><![CDATA[{1}]]></string>'.format(name, content)
 c = '{0}TextView.setText(Html.fromHtml(getString(R.string.{0})));'.format(name)
 
 print(s)
