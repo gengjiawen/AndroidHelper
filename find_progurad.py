c1aa96aa4ee0c136f5586a7deedab9333a6d50f5
gengjiawen <technicalcute@gmail.com>
2016-08-14 23:19:04 +0800

    add proguard uti

---
+++ find_progurad.py
@@ -0,0 +1,21 @@
+import re
+
+
+def find_proguard_rules(proguard_file):
+    s = open(proguard_file, mode='r', encoding='utf-8').read()
+    # all_class = re.findall("Warning:.*referenced class (.*)", s)
+    # all_field = re.findall("Warning:.* in program class (.*)", s)
+    # all_type = all_class + all_field
+    tmp_all = re.findall("Warning:.*( in program|referenced) class (.*)", s)
+    all_type = {i[1] for i in tmp_all}
+    proguard_issue = sorted(all_type)
+    refined_issue = set()
+    for i in proguard_issue:
+        sec_dot_pos = i.find(".", i.find(".") + 1)
+        refined_issue.add(i[:sec_dot_pos])
+
+    print("\n".join(sorted(proguard_issue)))
+    print("\nrefined rule:")
+    print("\n".join(sorted(refined_issue)))
+    return proguard_issue
+
