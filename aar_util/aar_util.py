8a9591a91193c7e9d4b85758edc974ee1d4c0902
gengjiawen <technicalcute@gmail.com>
2016-08-18 22:40:19 +0800

    add aar util

---
+++ aar_util/aar_util.py
@@ -0,0 +1,50 @@
+import os
+import shutil
+import tempfile
+import zipfile
+
+from utils.file_util import get_files_by_re, gen_new_file_extension
+
+
+def get_aar_files(proj_dir, des_dir):
+    rel_aar_dir = r"build\outputs\aar"
+    aar_dirs = [os.path.join(proj_dir, i) for i in os.listdir(proj_dir) if os.path.isdir(os.path.join(proj_dir, i))]
+    aar_dirs = [os.path.join(i, rel_aar_dir) for i in aar_dirs if os.path.exists(os.path.join(i, rel_aar_dir))]
+    for i in aar_dirs:
+        file = os.listdir(i)[0]
+        debug_aar = os.path.join(i, file)
+        print(debug_aar)
+        os.makedirs(des_dir, exist_ok=True)
+        shutil.copyfile(debug_aar, os.path.join(des_dir, file))
+
+
+def using_local_aar(aar_dir):
+    # http://stackoverflow.com/a/24894387/1713757
+    # or you can just do it in android studio ui
+    s = "configurations.create(\"default\")\n"
+    for i in os.listdir(aar_dir):
+        if i.endswith("aar"):
+            print("aar:", i)
+            t = "artifacts.add(\"default\", file('{}'))\n".format(i)
+            s += t
+    print(s)
+    build_script = os.path.join(aar_dir, "build.gradle")
+    open(build_script, mode='w', encoding='utf-8').write(s)
+    aar_module_name = os.path.basename(aar_dir)
+    print("add this to setting.gradle: ")
+    print("include ':{}'".format(aar_module_name))
+    print("\nadd this to mudule using aars: ")
+    print("compile project(':{}')".format(aar_module_name))
+
+
+def extract_aar2jar(aar_dir):
+    aar_files = get_files_by_re(aar_dir, ".*aar")
+    for i in aar_files:
+        jar_name = gen_new_file_extension(i, "jar")
+        with zipfile.ZipFile(i, "r") as z:
+            temp_dir = tempfile.mkdtemp()
+            z.extract("classes.jar", temp_dir)
+            if os.path.exists(jar_name):
+                os.remove(jar_name)
+            shutil.move(os.path.join(temp_dir, "classes.jar"), jar_name)
+
--- utils/file_util.py
+++ utils/file_util.py
@@ -19,3 +19,94 @@ def get_files_by_extensions(folder, extensions=None, sort_by=None):
         files = sorted(files, key=sort_by)
     return files
 
+
+def get_folders_by_re(folder, match_pattern=None):
+    all_folders = list()
+    for root, dirs, files in os.walk(folder):
+        for i in dirs:
+            f = os.path.abspath(os.path.join(root, i))
+            all_folders.append(f)
+
+    if match_pattern:
+        all_folders = [i for i in all_folders if re.match(match_pattern, i)]
+
+    return all_folders
+
+
+def get_files_by_re(folder, match_pattern=None, time_range=None):
+    """
+    get list of files by re and time range
+    :param folder:
+    :param match_pattern:
+    :param time_range: recent created or updated
+    :return: files in absolute path meet with the requirement
+    """
+    def in_time_range(i):
+        m = os.path.getmtime(i)
+        return True if time.time() - m < time_range else False
+
+    all_files = list()
+    for root, dirs, files in os.walk(folder):
+        for i in files:
+            f = os.path.abspath(os.path.join(root, i))
+            all_files.append(f)
+    if match_pattern:
+        all_files = [i for i in all_files if re.match(match_pattern, os.path.basename(i))]
+    if time_range:
+        all_files = [i for i in all_files if in_time_range(i)]
+
+    return all_files
+
+
+def get_immdiate_dir(folder):
+    file_list = [os.path.abspath(os.path.join(folder, i)) for i in next(os.walk(folder))[1]]
+    return file_list
+
+
+def get_file_name_without_extension(file):
+    file_name = os.path.basename(os.path.splitext(file)[0])
+    return file_name
+
+
+def gen_new_file_extension(file, new_extension):
+    """
+    generate new file extension for a input file
+    :param file:
+    :param new_extension:
+    :return:
+    """
+    base_file, ext = os.path.splitext(file)
+    return base_file + "." + new_extension
+
+
+def copy_folder(src, dst, symlinks=False, ignore=None):
+    os.makedirs(dst, exist_ok=True)
+    try:
+        from os import scandir
+        for entry in os.scandir(src):
+            d = os.path.join(dst, entry.name)
+            if entry.is_dir():
+                shutil.copytree(entry.path, d, symlinks, ignore)
+            else:
+                shutil.copy2(entry.path, d)
+    except ImportError:
+        for item in os.listdir(src):
+            s = os.path.join(src, item)
+            d = os.path.join(dst, item)
+            if os.path.isdir(s):
+                shutil.copytree(s, d, symlinks, ignore)
+            else:
+                shutil.copy2(s, d)
+
+
+def move_folder(src, dst, symlinks=False, ignore=None):
+    """
+    move folder
+    :param src:
+    :param dst:
+    :param symlinks:
+    :param ignore:
+    """
+    copy_folder(src, dst, symlinks=symlinks, ignore=ignore)
+    shutil.rmtree(src)
+
